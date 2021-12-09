2be983561688a66075091852afc0b52de212dcc9
Eduard Bucari <eduard.bucari@gmail.com>
2021-12-10 01:36:37 +0300

    1.2. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,38 @@
+package ru.job4j.concurrent;
+
+import javax.swing.table.TableRowSorter;
+import java.time.Year;
+
+/**
+ * Режим ожидания.
+ * Задание:
+ * 1. Создайте класс ru.job4j.concurrent.Wget и метод main.
+ *
+ * 2. В методе main необходимо симулировать процесс загрузки.
+ *    Для этого воспользуйтесь особенностью вывода на консоль:
+ *    System.out.print("\rLoading : " + index  + "%");
+ *    - Метод print печатает символы в строку без перевода каретки.
+ *    - Символ \r указывает, что каретку каждый раз нужно вернуть в начало строки.
+ *    Это позволяет через промежуток времени обновить строчку.
+ *
+ * 3. Создайте нить внутри метода main. В теле метода создайте цикл от 0 до 100.
+ *    Через 1 секунду выводите на консоль информацию о загрузке.
+ *    Вывод должен быть с обновлением строки.
+ */
+public class Wget {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {
+                    try {
+                        for (int i = 0; i <= 100; i++) {
+                            Thread.sleep(1000);
+                            System.out.println("\rLoading : " + i  + "%");
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        first.start();
+    }
+}
