fc723ef8d8039a5114f25dee30d0ac39f5ef6af1
Eduard Bucari <eduard.bucari@gmail.com>
2021-12-12 16:04:07 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/io/OutPutFile.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class OutPutFile {
+
+    private final File file;
+
+    public OutPutFile(File file) {
+        this.file = file;
+    }
+
+    public void saveContent(String content) {
+        try (BufferedOutputStream out = new BufferedOutputStream(
+                new FileOutputStream(file))) {
+            for (int i = 0; i < content.length(); i += 1) {
+                out.write(content.charAt(i));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -0,0 +1,52 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+/**
+ * Visibility. Общий ресурс вне критической секции.
+ * Задание:
+ * 1. Ниже приведен код, класс парсера файла.
+ * 2. Поправьте код с ошибками в коде:
+ * - Избавиться от get set за счет передачи File в конструктор.
+ * - Ошибки в многопоточности. Сделать класс Immutable. Все поля final.
+ * - Ошибки в IO. Не закрытые ресурсы. Чтение и запись файла без буфера.
+ * - Нарушен принцип единой ответственности. Тут нужно сделать два класса.
+ * - Методы getContent написаны в стиле копипаста.
+ *   Нужно применить шаблон стратегия: content(Predicate^Character> filter).
+ *
+ *   Для решения, дополнительно создадим класс OutPutFile и интерфес Connector.
+ */
+public class ParseFile implements Connector {
+
+    private final File file;
+
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+    public String getContent() {
+        return content((i) -> true);
+    }
+
+    public String getContentWithoutUnicode() {
+        return content((i) -> i < 0x80);
+    }
+
+    @Override
+    public String content(Predicate<Character> filter) {
+        StringBuilder output = new StringBuilder();
+        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(file))) {
+            int data;
+            while ((data = in.read()) > 0) {
+                output.append((char) data);
+                if (filter.test((char) data)) {
+                    output.append((char) data);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return output.toString();
+    }
+}
