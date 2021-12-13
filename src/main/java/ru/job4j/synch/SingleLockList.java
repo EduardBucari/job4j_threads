5e335eb9cf25c7664be65dcb37d269ab64f94aab
Eduard Bucari <eduard.bucari@gmail.com>
2021-12-13 16:51:36 +0300

    4. ThreadSafe динамический список [#1105]

---
+++ src/main/java/ru/job4j/synch/SingleLockList.java
@@ -0,0 +1,52 @@
+package ru.job4j.synch;
+
+import net.jcip.annotations.ThreadSafe;
+import net.jcip.annotations.GuardedBy;
+
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ThreadSafe динамический список.
+ * Задание:
+ * Вам нужно создать коллекцию, которая будет корректно работать в многопоточный среде.
+ */
+@ThreadSafe
+public class SingleLockList<T> implements Iterable<T> {
+    @GuardedBy("this")
+    private final List<T> list;
+
+    public SingleLockList() {
+        this.list = new ArrayList<>();
+    }
+
+    public SingleLockList(List<T> list) {
+        this.list = copy(list);
+    }
+
+    public void add(T value) {
+        synchronized (this) {
+            list.add(value);
+        }
+    }
+
+    public T get(int index) {
+        synchronized (this) {
+            return list.get(index);
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        synchronized (this) {
+            return copy(this.list).iterator();
+        }
+    }
+
+    private List<T> copy(List<T> list) {
+        synchronized (this) {
+            return new ArrayList<>(list);
+        }
+    }
+}
