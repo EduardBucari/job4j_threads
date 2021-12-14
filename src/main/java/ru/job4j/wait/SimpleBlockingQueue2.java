575bb646c39ca97cfaeb11326e58660b11007a81
Eduard Bucari <eduard.bucari@gmail.com>
2021-12-15 02:55:11 +0300

    3. Junit тест для блокирующей очереди. [#68589 #240010](2)

---
+++ src/main/java/ru/job4j/wait/SimpleBlockingQueue2.java
@@ -0,0 +1,55 @@
+package ru.job4j.wait;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue2<T> {
+    private final Object monitor = this;
+    @GuardedBy("monitor")
+    private final Queue<T> queue = new LinkedList<>();
+    private final int count;
+
+    public SimpleBlockingQueue2() {
+        count = 5;
+    }
+
+    public SimpleBlockingQueue2(int count) {
+        this.count = count;
+    }
+
+    public void offer(T value) throws InterruptedException {
+        synchronized (monitor) {
+            while (count == queue.size()) {
+                monitor.wait();
+            }
+            queue.add(value);
+            monitor.notifyAll();
+        }
+    }
+
+    public T poll() throws InterruptedException {
+        synchronized (monitor) {
+            while (queue.size() == 0) {
+                monitor.wait();
+            }
+            var rsl = queue.poll();
+            monitor.notifyAll();
+            return rsl;
+        }
+    }
+
+    public int size() {
+        synchronized (monitor) {
+            return queue.size();
+        }
+    }
+
+    public boolean isEmpty() {
+        synchronized (monitor) {
+            return queue.isEmpty();
+        }
+    }
+}
