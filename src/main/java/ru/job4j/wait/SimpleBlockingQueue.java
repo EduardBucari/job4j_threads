41023c7ef0a4a63a15c03807ba7d64d1a606e1ed
Eduard Bucari <eduard.bucari@gmail.com>
2021-12-15 14:48:46 +0300

    3. Junit тест для блокирующей очереди. [#68589 #240010](3)

---
+++ src/main/java/ru/job4j/wait/SimpleBlockingQueue.java
@@ -0,0 +1,55 @@
+package ru.job4j.wait;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+    private final Object monitor = this;
+    @GuardedBy("monitor")
+    private final Queue<T> queue = new LinkedList<>();
+    private final int count;
+
+    public SimpleBlockingQueue() {
+        count = 5;
+    }
+
+    public SimpleBlockingQueue(int count) {
+        this.count = count;
+    }
+
+    public void offer(T value) throws InterruptedException {
+        synchronized (monitor) {
+            while (count == queue.size()) {
+                monitor.wait();
+            }
+            queue.add(value);
+            monitor.notifyAll();
+        }
+    }
+
+    public T poll() throws InterruptedException {
+        synchronized (monitor) {
+            while (queue.size() == 0) {
+                monitor.wait();
+            }
+            var rsl = queue.poll();
+            monitor.notifyAll();
+            return rsl;
+        }
+    }
+
+    public int size() {
+        synchronized (monitor) {
+            return queue.size();
+        }
+    }
+
+    public boolean isEmpty() {
+        synchronized (monitor) {
+            return queue.isEmpty();
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/wait/SimpleBlockingQueueTest.java
@@ -0,0 +1,104 @@
+package ru.job4j.wait;
+
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SimpleBlockingQueueTest {
+
+    @Test
+    public void whenQueueIsFull() throws InterruptedException {
+        int maxSize = 3;
+        SimpleBlockingQueue<Integer> sbq = new SimpleBlockingQueue<>(maxSize);
+        Thread producer = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    sbq.offer(10);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        producer.start();
+        Thread.sleep(100);
+        producer.interrupt();
+        producer.join();
+        assertThat(sbq.size(), is(maxSize));
+    }
+
+    @Test
+    public void whenJob4jTest() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(1);
+        Thread producer = new Thread(
+                () -> IntStream.range(0, 5)
+                        .forEach(
+                                i -> {
+                                    try {
+                                        queue.offer(i);
+                                    } catch (InterruptedException e) {
+                                        Thread.currentThread().interrupt();
+                                    }
+                                })
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
+    }
+
+    @Test
+    public void whenArrayOfferPollTest() throws InterruptedException {
+        String[] words = new String[] {"Java", "is", "hard", ",", "but", "I", "really", "love", "it"};
+        SimpleBlockingQueue<String> sbq = new SimpleBlockingQueue<>(3);
+        String[] buffer = new String[words.length];
+        Thread producer = new Thread(
+                () -> Arrays.stream(words).forEach(
+                        i -> {
+                            try {
+                                sbq.offer(i);
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                )
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!sbq.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        for (int i = 0; i < buffer.length; i++) {
+                            try {
+                                buffer[i] = sbq.poll();
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(words, is(buffer));
+    }
+}
