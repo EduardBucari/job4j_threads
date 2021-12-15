4c3201b8c546734b0701bca9037c08aabfe3c433
Eduard Bucari <eduard.bucari@gmail.com>
2021-12-16 00:04:25 +0300

    0. CAS - операции [#6859 #240186](2)

--- src/main/java/ru/job4j/cas/CASCount.java
+++ src/main/java/ru/job4j/cas/CASCount.java
@@ -8,13 +8,16 @@ import java.util.concurrent.atomic.AtomicReference;
 public class CASCount {
     private final AtomicReference<Integer> count = new AtomicReference<>(0);
 
+    public CASCount(int count) {
+        this.count.set(count);
+    }
+
     public void increment() {
-        int ref = count.get();
-        int expected;
+        int ref;
         do {
-            expected = ref + 1;
+            ref = count.get();
         }
-        while (!count.compareAndSet(ref, expected));
+        while (!count.compareAndSet(ref, ref + 1));
     }
 
     public int get() {
