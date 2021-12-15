cc1fe940b740510fb49e2cf1fba313e5fe329326
Eduard Bucari <eduard.bucari@gmail.com>
2021-12-16 02:47:08 +0300

    1. Реализовать ThreadPool [#1099]

---
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -0,0 +1,54 @@
+package ru.job4j.pool;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import ru.job4j.wait.SimpleBlockingQueue;
+
+public class ThreadPool {
+
+    private final int size = Runtime.getRuntime().availableProcessors();
+    private final List<Thread> threads = new LinkedList<>();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(2);
+
+    public ThreadPool() {
+        for (int i = 0; i < size; i++) {
+            threads.add(new Thread(() -> {
+                try {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        System.out.println("I was born: " + Thread.currentThread().getId());
+                        tasks.poll().run();
+                    }
+                } catch (Exception e) {
+                    Thread.currentThread().interrupt();
+                }
+            }));
+        }
+        for (Thread t : threads) {
+            t.start();
+        }
+    }
+
+    public void work(Runnable job) throws InterruptedException {
+        tasks.offer(job);
+    }
+
+    public void shutdown() {
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadPool threadPool = new ThreadPool();
+        threadPool.work(() -> System.out.println("1 " + Thread.currentThread().getId()));
+        threadPool.work(() -> System.out.println("2 " + Thread.currentThread().getId()));
+        threadPool.work(() -> System.out.println("3 " + Thread.currentThread().getId()));
+        threadPool.work(() -> System.out.println("4 " + Thread.currentThread().getId()));
+        threadPool.work(() -> System.out.println("5 " + Thread.currentThread().getId()));
+        threadPool.work(() -> System.out.println("6 " + Thread.currentThread().getId()));
+        threadPool.work(() -> System.out.println("7 " + Thread.currentThread().getId()));
+        threadPool.work(() -> System.out.println("8 " + Thread.currentThread().getId()));
+        threadPool.shutdown();
+    }
+}
