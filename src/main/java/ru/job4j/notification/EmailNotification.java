0f4ec4e054239354b09c2f340e64dec6f3a9d179
Eduard Bucari <eduard.bucari@gmail.com>
2021-12-16 19:41:22 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/notification/EmailNotification.java
@@ -0,0 +1,46 @@
+package ru.job4j.notification;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Сервис для рассылки почти
+ */
+public class EmailNotification {
+    private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    /**
+     * Метод emailTo() должен отправлять почту через ExecutorService.
+     * @param user пользователь с email
+     */
+    public void emailTo(User user) {
+        var email = user.getEmail();
+        var username = user.getUsername();
+        var subject = String.format("Notification %s to email %s", username, email);
+        var body = String.format("Add a new event to %s", email);
+        pool.submit(() -> send(subject, body, email));
+    }
+
+    /**
+     * Метод send() отправляет email
+     * @param subject заголовок email
+     * @param body текст email
+     * @param email email конкретного пользователя.
+     */
+    public void send(String subject, String body, String email) {
+    }
+
+    /**
+     * Метод close() закрывает пул и ждёт пока завершаться все задачи.
+     */
+    public void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
